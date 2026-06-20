0fd43e5e6e7e8da4074115ccec1f4f3d0686d141
amansalmani073 <amansalmani073@gmail.com>
2026-06-20 12:51:00 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,5 +1,3 @@
-```javascript
-// Firebase SDK Imports
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-app.js";
 
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-auth.js";
@@ -8,39 +6,19 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.2/firebas
 
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-storage.js";
 
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.13.2/firebase-analytics.js";
-
-
-// Your Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDuwzfLoj2En-e93gCN-SXdCbBYgyblws8",
-    authDomain: "articlefeed-4be15.firebaseapp.com",
-    projectId: "articlefeed-4be15",
-    storageBucket: "articlefeed-4be15.firebasestorage.app",
-    messagingSenderId: "754859270659",
-    appId: "1:754859270659:web:d4268cfd1731beb5dba436",
-    measurementId: "G-HHCGDNTCPR"
+  apiKey: "AIzaSyDuwzfLoj2En-e93gCN-SXdCbBYgyblws8",
+  authDomain: "articlefeed-4be15.firebaseapp.com",
+  projectId: "articlefeed-4be15",
+  storageBucket: "articlefeed-4be15.firebasestorage.app",
+  messagingSenderId: "754859270659",
+  appId: "1:754859270659:web:d4268cfd1731beb5dba436"
 };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
-// Services
 const auth = getAuth(app);
-
 const db = getFirestore(app);
-
 const storage = getStorage(app);
 
-const analytics = getAnalytics(app);
-
-
-// Export Services
-export {
-    auth,
-    db,
-    storage
-};
-```
+export { auth, db, storage };
